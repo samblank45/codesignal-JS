775c7086e937a13626ba9d2ac3ff96f6163f0b94
sam <sblan45@gmail.com>
2021-02-27 18:47:52 -0500

    adds solution to problem 7

--- codesignal.js
+++ codesignal.js
@@ -38,19 +38,30 @@
 // console.log(makeArrayConsecutive2([1,4,15])) // 2  missing 2,4
 
 //problem 7
-
 function almostIncreasingSequence(sequence) {
-  let counter = 0
-  for (let i = 1; i < sequence.length; i++) {
-    console.log(sequence[i], 'i')
-    console.log(sequence[i -1], 'i-1')
-    if ( sequence[i] <= sequence[i -1]) {
-      counter++
-    }
-      
-  }
-  console.log(counter)
+  let count = 0;
+
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] <= sequence[i-1]) {
+      count++;
+      if (sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) {
+        console.log('--------')
+        console.log(sequence[i], '<=', sequence[i - 2], 'current index:', i)
+        console.log(sequence[i + 1], '<=', sequence[i - 1])
+        count++;
+      }
+    } 
+  } 
+  return count <= 1;
 }
+
+console.log(almostIncreasingSequence([1, 2, 3, 5])) //true
+console.log(almostIncreasingSequence([1, 2, 5, 3, 5, 3, 5, 1])) //false
+console.log(almostIncreasingSequence([1, 2, 3, 1, 2, 3])) //false
+console.log(almostIncreasingSequence([10,1,2,3,4,5])) //true
+console.log(almostIncreasingSequence([1, 2, 3, 4, 5, 3, 5, 6])) //false
+
+
   // let increasingSize = []
   // let sameArr = []
   // let highestNum = sequence[0]
@@ -118,7 +129,6 @@ function almostIncreasingSequence(sequence) {
 // }
 // input is an array output is a boolean
 
-console.log(almostIncreasingSequence([1, 2, 5, 3, 5])) //true
 // console.log(almostIncreasingSequence([1, 3, 2, 1])) //false
 // console.log(almostIncreasingSequence([10,20,30,40, 10, 20, 30])) //false
 // console.log(almostIncreasingSequence([40, 50, 60, 10, 20, 30])) //false
