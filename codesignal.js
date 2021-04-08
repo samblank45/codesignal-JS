001de5ed13280e1230fd01d125103153dfa4f3f2
sam <sblan45@gmail.com>
2021-04-07 23:44:08 -0400

    adds solution to problem 14 codesignal

--- codesignal.js
+++ codesignal.js
@@ -253,40 +253,23 @@ console.log(reverseInParentheses('foo(bar)')) // foorab
 console.log(reverseInParentheses('foo(bar(baz))blim')) // foobazrabblim
 // (baz)becomes zab  and then (barzab) becomes bazrab 
 
-const values = {
-  '01bbb998-af3d-47a4-b0ff-e67d033d80e9': 'Luz Ballard',
-  '01021494-f157-4183-964c-6b0ddc964ab8': 'Corey Johnson',
-  '97a2daa4-406b-4b1c-831e-bdfd90b224f2': 'Andrew Torres',
-  '6256ba85-b59f-40b9-8715-406cc54d7f05': 'Nichole Wilson',
-  '5c0c746c-ec56-4fb7-8b32-066b64d70611': 'Nancy Hall',
-  'c103b480-8efb-450f-9141-6a8037de2348': 'Agnes Lorenzen',
-  '4e0cc3dc-fce9-45d9-85c7-a3ae5cb0ce57': 'Donald Hyde',
-  'f80af139-5c68-4475-8cb6-ced7e38c6f43': 'Dennis Fuller',
-  '5073359e-b228-4852-b1a3-3f2edfc8672f': 'Francis Hodgkins',
-  '9c9a3cc8-044e-43d0-87ff-58a6b44eca53': 'David McLain'
-}
 
-function mutateObject(object) {
-  const entries = Object.entries(object)
-  let index = 0
-  let newObj = []
+//problem 14 alternatingSums
+// You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
 
-  for (const [id, name] of entries) {
-    newObj.push({'employeeId': id, 'name': name, 'id': index++, 'checked': false})
+function alternatingSums(a) {
+  let team1 = 0
+  let team2 = 0
+  let teams = []
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 === 0) {
+      team1 += a[i]
+    } else if (i % 2 === 1) {
+      team2 += a[i]
+    }
   }
-  return newObj
+  teams.push(team1, team2)
+  return teams
 }
 
-
-console.log('======')
-console.log(mutateObject(values))
-
-function test() {
-  let numbers = []
-  for ( let i = 0; i < 5; i++) {
-    numbers.push(Math.random() * (100 - 0))
-  }
-  return numbers
-}
-
-console.log(test(), 'test()')
+console.log(alternatingSums([1,0,3,4])) // [4,4]
