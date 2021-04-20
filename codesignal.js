5a84ec6f724e64e3adc0d44e419910e18c7f112b
sam <sblan45@gmail.com>
2021-04-20 18:42:43 -0400

    adds solution to fizzbuzz

--- codesignal.js
+++ codesignal.js
@@ -319,3 +319,11 @@ function duplicate(numbers) {
 }
 
 console.log(duplicate([1,2,3,4,5])) // [1,2,3,4,5,1,2,3,4,5]
+
+// Create a for loop that iterates up to 100 while outputting “fizz” at multiples of 3, “buzz” at multiples of 5 and “fizzbuzz” at multiples of 3 and 5
+
+for ( let i = 1; i < 100; i++) {
+  console.log(i)
+  i % 3 === 0 && i % 5 === 0 ? console.log(i, "fizzbuzz") : i % 5 === 0 ? console.log(i, "buzz") : i % 3 === 0 ? console.log(i, "fizz") : null
+}
+// no expected input, expected output is a string 
